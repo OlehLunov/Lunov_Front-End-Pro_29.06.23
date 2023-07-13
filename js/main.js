c83f17ad31661ea0bfc718944fecfd9f95058c1e
OlehLunov <olegap2701@gmail.com>
2023-07-13 21:14:10 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
-{
-    let hours = prompt("Введіть кількість годин:");
+
+    let hours = +prompt("Введіть кількість годин:");
 
     let seconds = hours * 3600;
 
     alert(`У ${hours} годинах - ${seconds} секунд.`);
-
-}
