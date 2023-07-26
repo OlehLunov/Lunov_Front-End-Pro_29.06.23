3ac32370dc908be5d4342dcb092bc3c635cccf3e
OlehLunov <olegap2701@gmail.com>
2023-07-26 23:24:44 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,68 +1,20 @@
-let step = "";
+let length = prompt("Введіть довжину массиву:");
 
-for (let i = 20; i <= 30; i += 0.5) {
-    step += i.toFixed(1) + ", ";
+let arr = [];
+
+for (let i = 0; i < length; i++) {
+    let element = prompt("Введіть елемент масиву " + (i + 1) + ":");
+    arr.push(element);
 }
-console.log(step.slice(0,-2));
+alert("Введений вами массив: " + arr.join(", ") + "!");
 
-/*-------dollar---------*/
-
-const course = 27;
-
-for (let i = 10; i <= 100; i += 10){
-    let hryvnia = i * course;
-    console.log(`${i} долларів дорівнює ${hryvnia} гривень`);
-}
-
-/*-------цілі числа-------*/
-
-let num = +prompt("Введіть ціле число:");
-
-for (let i = 1; i <= 100; i++) {
-    let square = i**2;
-    if (square <= num) {
-        console.log(i);
-    } else {
-        break;
+let choseElement = prompt("Введіть введіть елемент массиву який хочете видалити:");
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+    if (index !== -1) {
+        array.splice(index, 1);
     }
 }
-/*-------Просте число-----*/
+removeElement(arr, choseElement);
 
-let inputNum = +prompt("Введіть число:");
-let primeNum = true;
-
-if (inputNum <= 1){
-    primeNum = false;
-} else {
-    for (let i = 2; i ** 2 <= inputNum; i++) {
-        if (inputNum % i === 0) {
-            primeNum = false;
-            break;
-        }
-    }
-}
-if (primeNum) {
-    console.log("Просте.");
-} else {
-    console.log("Не  просте.");
-}
-
-/*---------------------------------------*/
-
-let raising = +prompt("Ввести число:");
-let saveRaising = raising;
-
-while (raising > 1 && raising % 3 === 0) {
-    raising /= 3;
-}
-if (raising === 1) {
-    console.log(`${saveRaising} - можна`);
-} else {
-    console.log(`${saveRaising} - не можна`);
-}
-
-
-
-
-
-
+alert("Змінений масив після видалення елемента: " + arr.join(", ") + "!");
