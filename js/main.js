6c47a2de798fa6c0c9921968d5847379fa4a0a2a
OlehLunov <olegap2701@gmail.com>
2023-07-26 23:54:07 +0300

    some 11

--- js/main.js
+++ js/main.js
@@ -1,19 +1,13 @@
-let length = prompt("Введіть довжину массиву:");
+const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
 
-let arr = [];
+let sumPos = 0;
+let countPos = 0;
 
-for (let i = 0; i < length; i++) {
-    let element = prompt("Введіть елемент масиву " + (i + 1) + ":");
-    arr.push(element);
+for (i =0; i < arr.length; i ++){
+    if(arr[i] >0) {
+        sumPos += arr[i];
+        countPos++;
+    }
 }
-alert("Введений вами массив: " + arr.join(", ") + "!");
-
-let choseElement = prompt("Введіть елемент массиву який хочете видалити:");
-
-function removeElement(array, item) {
-    const index = array.indexOf(item);
-        array.splice(index, 1);
-}
-removeElement(arr, choseElement);
-
-alert("Змінений масив після видалення елемента: " + arr.join(", ") + "!");
+console.log(`Сума позитивних елементів: ${sumPos}`);
+console.log(`Кількість позитивних елементів: ${countPos}`);
