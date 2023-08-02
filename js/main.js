4423643963228a1404966de57436e772032c70c7
OlehLunov <olegap2701@gmail.com>
2023-08-02 19:40:56 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
-const arr = [16,-37,54,-4,72,-56,47,4, -16,25,-37,46,4,-51,27,-63,4,-54,76,-4,12,-35,4,47];
+let length = +prompt("Введіть кількість елементів ключа:");
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+const key = generateKey(length, characters);
 
-let sumPos = 0;
-let countPos = 0;
+function generateKey (length, characters) {
 
-for (i =0; i < arr.length; i ++){
-    if(arr[i] >0) {
-        sumPos += arr[i];
-        countPos++;
+    const charactersArray = characters.split('');
+    let key = "";
+    for ( let i = 0; i < length; i++){
+        const randomElement = Math.floor(Math.random() * charactersArray.length);
+        key += charactersArray[randomElement];
     }
+    return key;
 }
-console.log(`Сума позитивних елементів: ${sumPos}`);
-console.log(`Кількість позитивних елементів: ${countPos}`);
+alert(`Ваш сгенерований код: ${key} !`);
+
