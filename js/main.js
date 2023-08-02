a3d0df92ee09b2f2b816a5e54074b77e36a3365c
OlehLunov <olegap2701@gmail.com>
2023-08-02 21:23:49 +0300

    done

--- js/main.js
+++ js/main.js
@@ -1,16 +1,85 @@
-let length = +prompt("Введіть кількість елементів ключа:");
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const key = generateKey(length, characters);
+const mainArray = [6, 2, 'hello', 7, 'world', 2, 5];
+const result = numbersAverage(mainArray);
 
-function generateKey (length, characters) {
+function numbersAverage (array) {
+    let sum = 0;
+    let count = 0;
 
-    const charactersArray = characters.split('');
-    let key = "";
-    for ( let i = 0; i < length; i++){
-        const randomElement = Math.floor(Math.random() * charactersArray.length);
-        key += charactersArray[randomElement];
+    for (let i = 0; i < array.length; i++){
+        const element = array[i];
+
+        if (typeof element === 'number'){
+            sum += element;
+            count++;
+        }
     }
-    return key;
+    return (sum/count);
 }
-alert(`Ваш сгенерований код: ${key} !`);
+console.log (result);
 
+
+
+
+
+const x = +prompt('Введіть перше число:');
+const znak = prompt('Введіть знак (+, -, *, /, %, ^):');
+const y = +prompt('Введіть друге число:');
+const mathResult = doMath(x, znak, y);
+
+function doMath(x, znak, y) {
+    if (znak === '+'){
+        return x + y;
+    } else if ( znak === '-'){
+        return x-y;
+    } else if (znak === '*'){
+        return x * y;
+    } else if (znak === '/'){
+        return x / y;
+    } else if (znak === '%'){
+        return x % y;
+    } else if (znak === '^'){
+        return x ** y;
+    } else{
+        return "Помилка";
+    }
+}
+alert(`Резултат вашого виразу: ${mathResult}`);
+
+
+
+
+
+let  mainArrayLength = +prompt('Введіть довжину основного масиву:');
+let  secondaryArrayLength = +prompt('Введіть довжину внутрішнього масиву:');
+const arrResult = twoArr;
+
+function twoArr() {
+
+    const mainArr = [];
+
+    for (let i = 0; i < mainArrayLength; i++){
+        const secondaryArray =[];
+        for (let k = 0; k < secondaryArrayLength; k++){
+            const element = +prompt(`Введіть елементи внутрішніх массивів:`);
+            secondaryArray.push(element);
+        }
+        mainArr.push(secondaryArray);
+    }
+    return mainArr;
+}
+
+console.log(arrResult());
+
+
+
+const newString = prompt('Введіть рядок:');
+const deleteLetter = prompt('Введіть символи, які потрібно видалити:');
+const deleteResult = removeLetters(newString,deleteLetter);
+function removeLetters(newString,deleteLetter) {
+    for (let i = 0; i < deleteLetter.length; i++){
+        const element = deleteLetter[i];
+        newString = newString.split(element).join('');
+    }
+    return newString;
+}
+alert(deleteResult);
