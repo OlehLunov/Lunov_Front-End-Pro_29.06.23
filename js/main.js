baf28d57e3fedaff590d1fe72dedf23afa4dd28b
Oleh Lunev <olegap2701@gmail.com>
2023-08-15 16:17:49 +0300

    some fixes need

--- js/main.js
+++ js/main.js
@@ -1,17 +1,23 @@
-let ladder = {
-    step: 0,
-    up: function () {
-        this.step++;
-        return this;
-    },
-    down: function () {
-        this.step--;
-        return this;
-    },
-    showStep: function () {
-        alert(this.step);
-        return this;
+class Human {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
     }
-};
+}
+class Car {
+    constructor(brand, model, manufacture, color, owner) {
+        this.brand = brand;
+        this.model = model;
+        this.manufacture = manufacture;
+        this.color = color;
+        this.owner = owner;
 
-ladder.up().up().down().showStep(); // 1
+        if (this.owner.age >= 18){
+            console.log(`Автомобіль: ${this.brand}, ${this.model}, ${this.manufacture}, ${this.color}.`);
+            console.log(`Властник: ${this.owner.name}, ${this.owner.age} - років`);
+            console.log(`Автомобіль: ${this.brand}, ${this.model}, ${this.manufacture}, ${this.color}.`);
+        } else {
+            console.log(`${this.owner.name} треба потерпіти ще один рік!`);
+        }
+    }
+}
