fad163c68c4bbdfc6297aa4423326c72db503b60
Oleh Lunev <olegap2701@gmail.com>
2023-08-23 12:13:25 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,17 @@
-const textField = document.getElementById("input");
-const someDiv = document.getElementById("some__div");
+const table = document.createElement("table");
 
-/*textField.addEventListener("focus", function () {
-   someDiv.style.display = "block";
-});
+let rows = 10;
+let cols = 10;
+let value = 1;
 
-textField.addEventListener( "blur", function (){
-    someDiv.style.display = "none";
-});*/
-
-textField.addEventListener("mouseover", function () {
-   someDiv.style.display = "block";
-});
-
-textField.addEventListener( "mouseout", function (){
-    someDiv.style.display = "none";
-});
-
-
+for ( let i = 0; i < rows; i++){
+    const row = document.createElement("tr");
+    for ( let k = 0; k < cols; k++){
+        const cell = document.createElement("td");
+        cell.textContent = value.toString();
+        row.appendChild(cell);
+        value++;
+    }
+    table.appendChild(row);
+}
+document.body.appendChild(table);
