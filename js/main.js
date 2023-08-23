93c96f68731ae378b4d0c73d36aa1134c9eb98d5
OlehLunov <olegap2701@gmail.com>
2023-08-23 21:38:16 +0300

    !

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 const categoryAdd = document.getElementsByClassName("nav__link");
 const productList = document.getElementsByClassName("products")[0];
+const info = document.getElementsByClassName("info")[0];
+const clear = document.getElementsByClassName("info_btn")[0];
+
 
 for (let i = 0; i < categoryAdd.length; i++) {
     categoryAdd[i].addEventListener("click", function () {
         productList.style.display = "block";
     });
 }
+productList.addEventListener("click", function () {
+    info.style.display = "block";
+});
+
+clear.addEventListener("click", function () {
+    alert("Товар добавлен в корзину");
+    info.style.display = "none";
+    productList.style.display = "none";
+})
