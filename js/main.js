4913a71fb9467da829030003c25fc0109699c7e7
OlehLunov <olegap2701@gmail.com>
2023-08-31 00:05:23 +0300

    2

--- js/main.js
+++ js/main.js
@@ -8,9 +8,7 @@ const products = [
     { name: 'IPAD', category: 'tablets', text: '1111', price: '1111' },
     { name: 'LENOVO', category: 'tablets', text: '2222', price: '22222' },
     { name: 'REDMI', category: 'tablets', text: '3333', price: '3333' },
-
 ];
-
 const smartsLink = document.querySelector('.smart__link');
 const phones = document.querySelector('.phones');
 const laptopsLink = document.querySelector('.laptops__link');
@@ -18,98 +16,81 @@ const laptops = document.querySelector('.laptops');
 const tabletsLink = document.querySelector('.tablets__link');
 const tablets = document.querySelector('.tablets');
 
-
 smartsLink.addEventListener('click', function() {
     phones.style.display = 'block';
     laptops.style.display = "none";
     tablets.style.display = "none";
 });
-
 laptopsLink.addEventListener('click', function() {
     laptops.style.display = 'block';
     phones.style.display = "none";
     tablets.style.display = "none";
 });
-
 tabletsLink.addEventListener('click', function() {
     tablets.style.display = 'block';
     laptops.style.display = "none";
     phones.style.display = "none";
 });
 
+const phonesItem = document.querySelector('.phones');
+const laptopsItem = document.querySelector('.laptops');
+const tabletsItem = document.querySelector('.tablets');
 const productInfo = document.querySelector('.product__info');
 const nameInfo = productInfo.querySelector('.name');
 const categoryInfo = productInfo.querySelector('.category');
 const textInfo = productInfo.querySelector('.text');
-const priceButton = productInfo.querySelector('.price');
+const buyButton = productInfo.querySelector('.price');
 
-function updateProduct(name, category, text, price) {
-    nameInfo.textContent = name;
-    categoryInfo.textContent = category;
-    textInfo.textContent = text;
-    priceButton.textContent = `Купити за ${price}`;
-    priceButton.addEventListener('click', () => {
-        alert(`Товар ${name} додано до кошика!`);
+products.forEach(product => {
+    const link = document.createElement('a');
+    link.href = '#';
+    link.textContent = product.name;
+    link.classList.add(`${product.name.toLowerCase()}-link`);
+
+    const listItem = document.createElement('li');
+    listItem.classList.add('list-item');
+    listItem.appendChild(link);
+
+    if (product.category === 'phones') {
+        phonesItem.appendChild(listItem);
+    } else if (product.category === 'laptops') {
+        laptopsItem.appendChild(listItem);
+    } else if (product.category === 'tablets') {
+        tabletsItem.appendChild(listItem);
+    }
+    link.addEventListener('click', function(event) {
+        event.preventDefault();
+        updateInfo(product.name, product.category, product.text, product.price);
     });
+});
+function updateInfo(name, category, text, buy) {
+    nameInfo.textContent = name;
+    categoryInfo.textContent = `Категорія: ${category}`;
+    textInfo.textContent = text;
+    buyButton.textContent = `Купити за ${buy}`;
     productInfo.style.display = 'block';
 }
 
-function resetProduct() {
-    nameInfo.textContent = '';
-    categoryInfo.textContent = '';
-    textInfo.textContent = '';
-    priceButton.textContent = 'Купити';
-    priceButton.removeEventListener('click', () => {});
-    productInfo.style.display = 'none';
-}
-
-const phoneLinks = document.querySelectorAll('.phones__list a');
-const laptopLinks = document.querySelectorAll('.laptops__list a');
-const tabletLinks = document.querySelectorAll('.tablets__list a');
-
-phoneLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const productName = this.textContent;
-        const product = products.find(p => p.name === productName);
-        updateProduct(product.name, product.category, product.text, product.price);
-    });
-});
-
-laptopLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const productName = this.textContent;
-        const product = products.find(p => p.name === productName);
-        updateProduct(product.name, product.category, product.text, product.price);
-    });
-});
-
-tabletLinks.forEach(link => {
-    link.addEventListener('click', function(event) {
-        event.preventDefault();
-        const productName = this.textContent;
-        const product = products.find(p => p.name === productName);
-        updateProduct(product.name, product.category, product.text, product.price);
-    });
-});
-
 const categoryLinks = document.querySelectorAll('.nav__list-item a');
-
 categoryLinks.forEach(link => {
     link.addEventListener('click', function(event) {
         event.preventDefault();
-        productInfo.style.display = 'none';
+        resetProductInfo();
     });
 });
-
-function hideAllProductBlocks() {
-    phones.style.display = 'none';
+buyButton.addEventListener('click', function() {
+    const productName = nameInfo.textContent;
+    alert(`Товар ${productName}, додано до кошика!`);
     laptops.style.display = 'none';
-    tablets.style.display = 'none';
-}
-
-priceButton.addEventListener('click', () => {
-    resetProduct();
-    hideAllProductBlocks();
+    phones.style.display = "none";
+    tablets.style.display = "none";
+    resetProductInfo();
 });
+
+function resetProductInfo() {
+    nameInfo.textContent = '';
+    categoryInfo.textContent = '';
+    textInfo.textContent = '';
+    buyButton.textContent = 'Купити';
+    productInfo.style.display = 'none';
+}
