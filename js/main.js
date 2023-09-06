e7ba27aeaee089690a2c083af5b8333ac6e07d65
OlehLunov <olegap2701@gmail.com>
2023-09-06 22:19:33 +0300

    done

--- js/main.js
+++ js/main.js
@@ -7,24 +7,28 @@ function showData(data) {
     let speed = data.wind.speed;
     let deg = data.wind.deg;
 
+    let iconCode = data.weather[0].icon;
+
+    let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+    document.getElementById("icon").src = iconUrl;
 
     document.getElementById("city").textContent = city;
-    document.getElementById("temp").textContent = temp;
+    document.getElementById("temp").textContent = `Температура: ${temp}`;
     document.getElementById("pressure").textContent = `Тиск: ${pressure}`;
-    document.getElementById("description").textContent = description;
-    document.getElementById("humidity").textContent = humidity;
-    document.getElementById("speed").textContent = speed;
-    document.getElementById("deg").textContent = deg;
+    document.getElementById("description").textContent = `Опис: ${description}`;
+    document.getElementById("humidity").textContent = `Вологість: ${humidity}`;
+    document.getElementById("speed").textContent = `Швидкість вітру: ${speed}`;
+    document.getElementById("deg").textContent = `Напрям у градусах: ${deg}`;
 }
 
 function fetchWeather() {
-    const weatherLink = "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19";
+    const weatherLink = "http://api.openweathermap.org/data/2.5/weather?q=Dnipro&units=metric&APPID=5d066958a60d315387d9492393935c19";
 
     fetch(weatherLink)
         .then(response => {
             if (response.ok) {
                 return response.json();
-            } else 
+            } else {
                 throw new Error('Запит не ОК');
             }
         })
@@ -37,3 +41,39 @@ function fetchWeather() {
 }
 
 fetchWeather();
+
+
+// const xhr = new XMLHttpRequest();
+
+// const weatherLink = "http://api.openweathermap.org/data/2.5/weather?q=Dnipro&units=metric&APPID=5d066958a60d315387d9492393935c19";
+
+// xhr.open("GET", weatherLink, true);
+
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4 && xhr.status === 200) {
+//         let data = JSON.parse(xhr.responseText);
+    
+//         let iconCode = data.weather[0].icon;
+
+//         let iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+//         document.getElementById("icon").src = iconUrl;
+
+//         let city = data.name;
+//         let temp = data.main.temp;
+//         let pressure = data.main.pressure;
+//         let description = data.weather[0].description;
+//         let humidity = data.main.humidity;
+//         let speed = data.wind.speed;
+//         let deg = data.wind.deg;
+
+//         document.getElementById("city").textContent = city;
+//         document.getElementById("temp").textContent = `Температура: ${temp}`;
+//         document.getElementById("pressure").textContent = `Тиск: ${pressure}`;
+//         document.getElementById("description").textContent = `Опис: ${description}`;
+//         document.getElementById("humidity").textContent = `Вологість: ${humidity}`;
+//         document.getElementById("speed").textContent = `Швидкість вітру: ${speed}`;
+//         document.getElementById("deg").textContent = `Напрям у градусах: ${deg}`;
+//     }
+// };
+// xhr.send();
+
