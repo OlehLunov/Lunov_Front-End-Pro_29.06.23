f2a9e6046e0bcadaf79c2f6e6b6e86746382dd26
OlehLunov <olegap2701@gmail.com>
2023-09-19 08:34:13 +0300

    111

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ const products = [
     { name: 'IPAD', category: 'tablets', text: '1111', price: '1111' },
     { name: 'LENOVO', category: 'tablets', text: '2222', price: '22222' },
     { name: 'REDMI', category: 'tablets', text: '3333', price: '3333' },
-
 ];
 
 const categoryNav = document.querySelector(".nav");
@@ -18,6 +17,17 @@ const laptopsLink = document.querySelector('.laptops__link');
 const laptops = document.querySelector('.laptops');
 const tabletsLink = document.querySelector('.tablets__link');
 const tablets = document.querySelector('.tablets');
+const phonesItem = document.querySelector('.phones');
+const laptopsItem = document.querySelector('.laptops');
+const tabletsItem = document.querySelector('.tablets');
+const productInfo = document.querySelector('.product__info');
+const nameInfo = productInfo.querySelector('.name');
+const categoryInfo = productInfo.querySelector('.category');
+const textInfo = productInfo.querySelector('.text');
+const buyButton = productInfo.querySelector('.price');
+const ordersBtn = document.querySelector(".orders__btn");
+const ordersDiv = document.querySelector(".orders");
+const ordersList = document.querySelector(".orders__list");
 
 smartsLink.addEventListener('click', function() {
     phones.style.display = 'block';
@@ -34,15 +44,6 @@ tabletsLink.addEventListener('click', function() {
     laptops.style.display = "none";
     phones.style.display = "none";
 });
-
-const phonesItem = document.querySelector('.phones');
-const laptopsItem = document.querySelector('.laptops');
-const tabletsItem = document.querySelector('.tablets');
-const productInfo = document.querySelector('.product__info');
-const nameInfo = productInfo.querySelector('.name');
-const categoryInfo = productInfo.querySelector('.category');
-const textInfo = productInfo.querySelector('.text');
-const buyButton = productInfo.querySelector('.price');
 
 products.forEach(product => {
     const link = document.createElement('a');
@@ -66,13 +67,15 @@ products.forEach(product => {
         updateInfo(product.name, product.category, product.text, product.price);
     });
 });
-function updateInfo(name, category, text, buy) {
+
+function updateInfo(name, category, text, price) {
     nameInfo.textContent = name;
     categoryInfo.textContent = `Категорія: ${category}`;
     textInfo.textContent = text;
-    buyButton.textContent = `Купити`;
+    buyButton.textContent = `Ціна: ${price} грн`; 
     productInfo.style.display = 'block';
 }
+
 
 const categoryLinks = document.querySelectorAll('.nav__list-item a');
 categoryLinks.forEach(link => {
@@ -81,24 +84,29 @@ categoryLinks.forEach(link => {
         resetProduct();
     });
 });
+
 buyButton.addEventListener('click', function() {
     const productName = nameInfo.textContent;
-    const orderTime = new Date().toLocaleString();
-    const priceToOrder = products.find(item => item.name.toUpperCase() === productName.toUpperCase());
-    const orderPriceAdd = priceToOrder ? priceToOrder.price : '';
+    const product = products.find(p => p.name === productName);
 
-    const orderItem = document.createElement('li');
-    orderItem.textContent = `${orderTime} - Ціна: ${orderPriceAdd}`;
-    orderList.appendChild(orderItem);
-
-    
-    alert(`Товар ${productName}, додано до кошика!`);
-    laptops.style.display = 'none';
-    phones.style.display = "none";
-    tablets.style.display = "none";
-    resetProduct();
+    if (product) {
+        const order = {
+            date: new Date().toLocaleString(),
+            price: product.price,
+            name: product.name,
+            category: product.category, 
+            text: product.text 
+        };
+        const savedOrders = JSON.parse(localStorage.getItem('orders')) || [];
+        savedOrders.push(order);
+        localStorage.setItem('orders', JSON.stringify(savedOrders));
+        laptops.style.display = 'none';
+        phones.style.display = "none";
+        tablets.style.display = "none";
+        resetProduct();
+        alert(`Товар ${productName} додано до кошика!`);
+    }
 });
-
 function resetProduct() {
     nameInfo.textContent = '';
     categoryInfo.textContent = '';
@@ -106,28 +114,54 @@ function resetProduct() {
     buyButton.textContent = 'Купити';
     productInfo.style.display = 'none';
 }
+ordersBtn.addEventListener('click', function() {
+    const savedOrders = JSON.parse(localStorage.getItem('orders')) || [];
 
-// ----------------------------------------------------------------------------------------
-
-const orderButton = document.querySelector(".orders__btn");
-const orderInfo = document.querySelector('.orders');
-const toMainBtn = document.querySelector(".tomain__btn");
-const productsCat = document.querySelector(".products");
-const orderList = document.querySelector(".order-list");
-
-
-
-orderButton.addEventListener('click', function() {
-    localStorage.removeItem('orders');
-   categoryNav.style.display = 'none';
-   orderInfo.style.display = 'block';
-   toMainBtn.style.display = "block";
-   productsCat.style.display = "none";
-   resetProduct();
-});
-
-toMainBtn.addEventListener("click", function(){
+    if (savedOrders.length === 0) {
         categoryNav.style.display = 'block';
-        orderInfo.style.display = 'none';
-        toMainBtn.style.display = "none";
-} )
+    } else {
+        categoryNav.style.display = 'none';
+    }
+
+    showOrders();
+});
+function showOrders() {
+    const savedOrders = JSON.parse(localStorage.getItem('orders')) || [];
+    ordersDiv.innerHTML = '';
+
+    savedOrders.forEach((order, index) => {
+        const product = products.find(product => product.name === order.name);
+    
+        const orderElement = document.createElement('div');
+        orderElement.classList.add('order');
+        orderElement.innerHTML = `
+            <div class="order-header">
+                <span class="order-date">Замовлення #${index + 1} - ${order.date}</span>
+                <button class="delete-order-btn" data-index="${index}">Видалити</button>
+            </div>
+            <div class="order-details" style="display: none;">
+                <p>Ціна: ${order.price} грн</p>
+                <div class="order-info">
+                    <p>Name: ${order.name}</p>
+                    <p>Category: ${order.category}</p>
+                    <p>Text: ${order.text}</p>
+                </div>
+            </div>
+        `;
+        orderElement.querySelector('.order-header').addEventListener('click', function() {
+            const details = orderElement.querySelector('.order-details');
+            if (details.style.display === 'none') {
+                details.style.display = 'block';
+            } else {
+                details.style.display = 'none';
+            }
+        });
+        orderElement.querySelector('.delete-order-btn').addEventListener('click', function() {
+            const index = this.getAttribute('data-index');
+            savedOrders.splice(index, 1);
+            localStorage.setItem('orders', JSON.stringify(savedOrders));
+            showOrders();
+        });
+        ordersDiv.appendChild(orderElement);
+    });
+}
