fb52135646fd765da919ef9ee34d59fbdfda4958
Oleh Lunev <olegap2701@gmail.com>
2023-09-22 14:30:46 +0300

    some react

---
+++ dz_27/src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import "../Header/Header.css"
+
+
+function Header() {
+return (
+
+    <nav className="header__nav">
+        <ul className="header_list">
+            <li className="header__item">
+                <a href="#" className="header__item-link">123214214</a>
+            </li>
+            <li className="header__item">
+                <a href="#" className="header__item-link">123214214</a>
+            </li>
+            <li className="header__item">
+                <a href="#" className="header__item-link">123214214</a>
+            </li>
+            <li className="header__item">
+                <a href="#" className="header__item-link">123214214</a>
+            </li>
+        </ul>
+    </nav>
+)
+}
+export default Header;
