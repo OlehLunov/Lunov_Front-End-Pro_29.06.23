4b5b52116c7573bd0e02bc53037a14e9cf6423fa
Oleh Lunev <olegap2701@gmail.com>
2023-09-22 15:19:04 +0300

    main

--- dz_27/src/App.js
+++ dz_27/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css"
 import Header from './components/Header/Header';
 import Aside from "./components/Aside/Aside";
+import Main from "./components/Main/Main";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
       <Aside />
+      
     </div>
   );
 }
---
+++ dz_27/src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import "../Main/Main.css"
+
+
+function Main() {
+return (
+<div className="main__content">
+    <p className="main__content-text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Neque, vel explicabo dolorem quas deserunt expedita illum 
+        amet facilis voluptate recusandae dolorum, asperiores repellendus 
+        quod nemo reprehenderit impedit praesentium deleniti quos?
+    </p>
+</div>      
+)
+}
+export default Main;
