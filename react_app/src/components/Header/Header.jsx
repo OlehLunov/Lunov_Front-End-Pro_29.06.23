02cca1bcdae7c2adc180df853f0e07cbe829e988
OlehLunov <olegap2701@gmail.com>
2023-11-17 00:06:42 +0200

    react

--- react_app/src/components/Header/Header.jsx
+++ react_app/src/components/Header/Header.jsx
@@ -4,16 +4,16 @@ import './Header.scss';
 const Header = () => {
   return (
     <header className="App-header">
-      <div className="logo">Your Logo</div>
+      <a className="logo">LOGO</a>
       <div className="links">
         <a href="#">Link 1</a>
         <a href="#">Link 2</a>
         <a href="#">Link 3</a>
         <a href="#">Link 4</a>
       </div>
-      <div className="buttons">
-        <button>Логин</button>
-        <button>Регистрация</button>
+      <div className="btn">
+        <button className='btn_log'>Логин</button>
+        <button className='btn_reg'>Регистрация</button>
       </div>
     </header>
   );
