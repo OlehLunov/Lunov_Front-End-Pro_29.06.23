1b04cb255a64d7c3c418aa3ae3dfd4e4b3fe3d13
OlehLunov <olegap2701@gmail.com>
2023-11-28 21:39:19 +0200

    35

--- dz_35/gulpfile.js
+++ dz_35/gulpfile.js
@@ -3,21 +3,24 @@ const babel = require('gulp-babel');
 const cleanCSS = require('gulp-clean-css');
 const uglify = require('gulp-uglify');
 
-
 gulp.task('styles', function() {
-    return gulp.src('./src/components/Main.css')
-      .pipe(cleanCSS()) 
-      .pipe(gulp.dest('content/css')); 
-  });
+  return gulp.src('./src/components/Main.css')
+    .pipe(cleanCSS()) 
+    .pipe(gulp.dest('content/css')); 
+});
 
-  gulp.task('scripts', function() {
-    return gulp.src('./src/components/Main.jsx')
-      .pipe(babel({
-        presets: ['@babel/preset-react'] 
-      }))
-      .pipe(uglify()) 
-      .pipe(gulp.dest('content/js')); 
-  });
+gulp.task('scripts', function() {
+  return gulp.src('./src/components/Main.jsx')
+    .pipe(babel({
+      presets: ['@babel/preset-react'] 
+    }))
+    .pipe(uglify()) 
+    .pipe(gulp.dest('content/js')); 
+});
 
-  gulp.task('default', gulp.parallel('styles', 'scripts'));
+gulp.task('watch', function() {
+  gulp.watch('./src/components/Main.css', gulp.series('styles'));
+  gulp.watch('./src/components/Main.jsx', gulp.series('scripts'));
+});
 
+gulp.task('default', gulp.parallel('styles', 'scripts', 'watch'));
